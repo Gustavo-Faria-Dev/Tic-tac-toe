c386fd1bfd86cae5c7d14d0688ce06c1d426b055
Gustavo Faria <gustavo.fariams@outlook.com>
2023-04-06 17:20:13 -0300

    Criado componente de ícone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,19 @@
 import styles from './header.module.css'
 
+
 import Title from "../title/Title"
 import Subtitle from "../subtitle/SubTitle"
+import Icon from '../icon/Icon'
+
 
 function Header (){
     return(
         <div className={styles.header}>
             <Title>Jogo da velha</Title>
             <Subtitle>Criado por Gustavo Faria</Subtitle>
+            <div className={styles.iconContent}>
+                <Icon iconName="github" link = "https://github.com/Gustavo-Faria-Dev"/>
+            </div>
         </div>
     )
     
